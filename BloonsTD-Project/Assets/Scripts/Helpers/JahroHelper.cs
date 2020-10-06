4f0993f49750a0bc7ca239cfae71d9fa438aca12
DESKTOP-I02B1D9\Johnny <jthompson59@mail.csuchico.edu>
2020-10-06 14:06:55 -0700

    Added Jahro Console to the project.

---
+++ BloonsTD-Project/Assets/Scripts/Helpers/JahroHelper.cs
@@ -0,0 +1,11 @@
+﻿using JahroConsole;
+using UnityEngine;
+
+public class JahroHelper
+{
+    [JahroCommand("test-command", "Sends a test command to the debug console")]
+    public static void TestCommand()
+    {
+        Debug.Log("Test command from Jahro Console.");
+    }
+}
