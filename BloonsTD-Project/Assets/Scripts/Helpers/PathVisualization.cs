17d8a1476d05f018a6cdef19ea421012e07c554b
DESKTOP-I02B1D9\Johnny <jthompson59@mail.csuchico.edu>
2020-10-18 18:59:23 -0700

    Added edges to the visualization path.

--- BloonsTD-Project/Assets/Scripts/Helpers/PathVisualization.cs
+++ BloonsTD-Project/Assets/Scripts/Helpers/PathVisualization.cs
@@ -12,17 +12,31 @@ namespace Helpers
         {
             if (_pathController == null ||
                 (_pathController.Waypoints == null || _pathController.Waypoints.Count == 0)) return;
-            
-            
+
+            var pathRadius = _pathController.PathWidth / 2f;
             
             for (int i = 0; i < _pathController.Waypoints.Count; i++)
             {
                 Gizmos.color = Color.yellow;
-                Gizmos.DrawWireSphere(_pathController.Waypoints[i].transform.position, _pathController.PathWidth);
+                Gizmos.DrawWireSphere(_pathController.Waypoints[i].transform.position, pathRadius);
+                
                 if (i == _pathController.Waypoints.Count - 1) break;
-                Gizmos.color = Color.green;
-                Gizmos.DrawLine(_pathController.Waypoints[i].transform.position,
-                    _pathController.Waypoints[i + 1].transform.position);
+                
+                var pathStartPos = _pathController.Waypoints[i].transform.position;
+                var pathEndPos = _pathController.Waypoints[i + 1].transform.position;
+
+                Gizmos.DrawLine(pathStartPos, pathEndPos);
+                
+                var path = pathEndPos - pathStartPos;
+                var pathCross = (Vector3.Cross(path, Vector3.forward)).normalized * pathRadius;
+                
+                var edge1Start = pathStartPos + pathCross;
+                var edge1End = edge1Start + path;
+                var edge2Start = pathStartPos - pathCross;
+                var edge2End = edge2Start + path;
+                
+                Gizmos.DrawLine(edge1Start, edge1End);
+                Gizmos.DrawLine(edge2Start, edge2End);
             }
         }
     }
