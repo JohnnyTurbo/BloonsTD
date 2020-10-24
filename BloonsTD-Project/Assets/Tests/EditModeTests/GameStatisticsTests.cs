2726b112dbc2b6706f7014fbbd33bffe1f6cc857
DESKTOP-I02B1D9\Johnny <jthompson59@mail.csuchico.edu>
2020-10-24 08:30:33 -0700

    Added Bloons Properties test for SetGameStatistics() method.

--- BloonsTD-Project/Assets/Tests/EditModeTests/GameStatisticsTests.cs
+++ BloonsTD-Project/Assets/Tests/EditModeTests/GameStatisticsTests.cs
@@ -39,6 +39,7 @@ namespace EditModeTests
             testGameStatistics.Rounds.Should().Be(50);
             testGameStatistics.Money.Should().Be(650);
             testGameStatistics.Lives.Should().Be(50);
+            testGameStatistics.MaxGameStatistics.Should().Be(maxGameStatistics);
         }
 
         [Test]
@@ -51,6 +52,20 @@ namespace EditModeTests
             testGameStatistics.Rounds.Should().Be(0);
             testGameStatistics.Money.Should().Be(0);
             testGameStatistics.Money.Should().Be(0);
+            testGameStatistics.MaxGameStatistics.Should().Be(maxGameStatistics);
+        }
+
+        [Test]
+        public void Test_Set_Game_Statistics()
+        {
+            GameStatistics gameStatisticsA = A.GameStatistics.WithRounds(25).WithMoney(200).WithLives(40);
+            GameStatistics gameStatisticsB = A.GameStatistics.WithRounds(50).WithMoney(650).WithLives(50);
+            
+            gameStatisticsA.SetGameStatistics(gameStatisticsB);
+
+            gameStatisticsA.Rounds.Should().Be(gameStatisticsB.Rounds);
+            gameStatisticsA.Money.Should().Be(gameStatisticsB.Money);
+            gameStatisticsA.Lives.Should().Be(gameStatisticsB.Lives);
         }
     }
 }
