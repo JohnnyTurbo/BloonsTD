5ed3b446e35e093a80feb67761d641b1b240745b
DESKTOP-I02B1D9\Johnny <jthompson59@mail.csuchico.edu>
2020-12-05 16:32:41 -0800

    Refactored BasicProjectileController.cs

--- BloonsTD-Project/Assets/Scripts/Gameplay/Hazards/BasicProjectileController.cs
+++ BloonsTD-Project/Assets/Scripts/Gameplay/Hazards/BasicProjectileController.cs
@@ -1,12 +1,13 @@
 using UnityEngine;
+using UnityEngine.Serialization;
 
 namespace TMG.BloonsTD.Gameplay
 {
     [RequireComponent(typeof(Rigidbody2D))]
     public class BasicProjectileController : Hazard
     {
-        [SerializeField] private float _speed;
-        [SerializeField] private float _maxRange;
+        [SerializeField] private float _movementSpeed;
+        [SerializeField] private float _maxDistanceTraveled;
         
         private Rigidbody2D _rigidbody;
         private Vector3 _startPosition;
@@ -21,12 +22,12 @@ namespace TMG.BloonsTD.Gameplay
         {
             base.Start();
             _startPosition = transform.position;
-            _rigidbody.AddForce(transform.up * _speed, ForceMode2D.Impulse);
+            _rigidbody.AddForce(transform.up * _movementSpeed, ForceMode2D.Impulse);
         }
 
         private void Update()
         {
-            if (DistanceTraveled > _maxRange)
+            if (DistanceTraveled > _maxDistanceTraveled)
             {
                 DestroyHazard();
             }
